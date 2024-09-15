321ee57f31de03956ee4e195111b9a1e8cdccfde
swapnil0000 <swapnilsrivastav1999@gmail.com>
2024-09-15 17:37:17 +0530

    second commit

--- config/server.js
+++ config/server.js
@@ -2,9 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('6386455982'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    keys: [env('APP_KEY1',c70b72553ce0e9e5b4c05c9624bbe005a8cdefac79151e75a5586c3f28b4d8bf), env('APP_KEY2',f231b79d14cdd29e5623b0f21ac5e5fa45ca30188dca0c392c4a579d32e2464a),],
   },
 });
